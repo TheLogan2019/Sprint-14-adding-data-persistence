2f99615a6a36da65e11215aca3a2afdac46a8627
Logan <thelogan2019@gmail.com>
2023-06-09 15:13:01 -0500

    working on routers

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,22 @@
-// build your `/api/projects` router here
+const router = require("express").Router();
+
+const Project = require("./model");
+
+router.get("/", (req, res, next) => {
+  Project.get()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch(next);
+});
+
+router.use((err, res, req, next) => {
+  //eslint-disable-line
+  res.status(500).json({
+    customMessage: "something went wrong!",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,12 @@
-// build your `/api/resources` router here
+const router = require("express").Router();
+
+router.use((err, res, req, next) => {
+  //eslint-disable-line
+  res.status(500).json({
+    customMessage: "something went wrong!",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
