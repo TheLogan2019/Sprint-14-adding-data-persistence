f9b846a8c250c2bafee4917877d467ec7fe87bef
Logan <thelogan2019@gmail.com>
2023-06-11 16:50:51 -0500

    resources and tasks done in db

--- data/migrations/20230611205106_initial-migration.js
+++ data/migrations/20230611205106_initial-migration.js
@@ -1,16 +1,32 @@
 exports.up = async function (knex) {
   await knex.schema
-    .createTable("projects", (table) => {
-      table.increments("project_id");
+    .createTable("projects", (tbl) => {
+      tbl.increments("project_id");
+      tbl.string("project_name", 200).notNullable().unique();
+      tbl.string("project_description", 200);
+      tbl.boolean("project_completed").defaultTo(false);
     })
-    .createTable("resources", (table) => {
-      table.increments("resource_id");
+    .createTable("resources", (tbl) => {
+      tbl.increments("resource_id");
+      tbl.string("resource_name", 200).notNullable().unique();
+      tbl.string("resource_description", 200);
     })
-    .createTable("tasks", (table) => {
-      table.increments("task_id");
+    .createTable("tasks", (tbl) => {
+      tbl.increments("task_id");
+      tbl.string("task_description", 200).notNullable();
+      tbl.string("task_notes", 200);
+      tbl.boolean("task_completed").defaultTo(false);
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
     })
-    .createTable("project_resources", (table) => {
-      table.increments("project_resources_id");
+    .createTable("project_resources", (tbl) => {
+      tbl.increments("project_resources_id");
     });
 };
 
