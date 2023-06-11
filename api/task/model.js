1855a0c18f6c92a6ba170b0cbb64bb9052d39cbf
Logan <thelogan2019@gmail.com>
2023-06-11 18:36:45 -0500

    fixed tasks and passed remaining tests

--- api/task/model.js
+++ api/task/model.js
@@ -11,13 +11,24 @@ async function getAllTasks() {
     "p.project_id",
     "t.project_id"
   );
-  return tasks;
+  const taskTest = tasks.map((task) => {
+    if (task.task_completed === 1) {
+      return { ...task, task_completed: true };
+    } else {
+      return { ...task, task_completed: false };
+    }
+  });
+  return taskTest;
 }
 
 async function createNewTask(task) {
   const [task_id] = await db("tasks").insert(task);
   const newTask = await db("tasks").where("task_id", task_id).first();
-  return newTask;
+  if (newTask.task_completed === 1 || newTask.task_completed === true) {
+    return { ...newTask, task_completed: true };
+  } else {
+    return { ...newTask, task_completed: false };
+  }
 }
 
 module.exports = { getTaskById, getAllTasks, createNewTask };
