235add7a0403de87597f55492c4f31e54e0db13c
Logan <thelogan2019@gmail.com>
2023-06-11 15:32:09 -0500

    finished tasks

--- api/task/model.js
+++ api/task/model.js
@@ -6,13 +6,18 @@ async function getTaskById(task_id) {
 }
 
 async function getAllTasks() {
-  const tasks = await db("tasks");
+  const tasks = await db("tasks as t").join(
+    "projects as p",
+    "p.project_id",
+    "t.project_id"
+  );
   return tasks;
 }
 
 async function createNewTask(task) {
   const [task_id] = await db("tasks").insert(task);
-  return await getTaskById(task_id);
+  const newTask = await db("tasks").where("task_id", task_id).first();
+  return newTask;
 }
 
 module.exports = { getTaskById, getAllTasks, createNewTask };
