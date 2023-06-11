864de33e8e91d9485322961b0187ff9e4fddd425
Logan <thelogan2019@gmail.com>
2023-06-10 20:57:11 -0500

    almost finished tasks

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,18 @@
-// build your `Task` model here
+const db = require("../../data/dbConfig");
+
+async function getTaskById(task_id) {
+  const task = await db("task").where(task_id).first();
+  return task;
+}
+
+async function getAllTasks() {
+  const tasks = await db("tasks");
+  return tasks;
+}
+
+async function createNewTask(task) {
+  const [task_id] = await db("tasks").insert(task);
+  return await getTaskById(task_id);
+}
+
+module.exports = { getTaskById, getAllTasks, createNewTask };
--- api/task/router.js
+++ api/task/router.js
@@ -1,6 +1,23 @@
 const router = require("express").Router();
 const Task = require("./model");
 
+router.get("/", (req, res, next) => {
+  Task.getAllTasks()
+    .then((tasks) => {
+      res.status(200).json(tasks);
+    })
+    .catch(next);
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newTask = await Task.createNewTask(req.body);
+    res.status(200).json(newTask);
+  } catch (err) {
+    next(err);
+  }
+});
+
 //prettier-ignore
 router.use((err, res, req, next) => {//eslint-disable-line
   res.status(500).json({
